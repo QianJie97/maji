c826466bf6c653e00d33905bdebc80cc56a74ea9
钱杰 <1501975941@qq.com>
2018-03-26 15:08:34 +0800

    commit

---
+++ utils/constant.js
@@ -0,0 +1,11 @@
+
+var brandId = 4717;
+var menuId = '135450';
+// var brandId = 5771;//正式
+// var menuId = '120891';//正式
+
+module.exports = {
+  brandId: brandId,
+  clientId: 'ac9784632be3529f6068af87cf5d0b92bf7022',
+  menuId: menuId
+}                                                                                                                                                                                                                                                                                                                                                                                                                                                                                         
